8430f47dac57db4a4aa8750515470083ae2c0462
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-24 18:46:40 +0200

    created a function that satisfies all the tests

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 function calculateBill(price, vat, tip){
-    
+    if(!(Number.isInteger(price))){
+        return "error";
+    }
+    if( !(Number.isInteger(vat)) ){
+        return "error";
+    }
+    if( !(Number.isInteger(tip)) ){
+        return "error";
+    }
+
+    const total = price + vat + tip;
+    return "£"+total.toString();
 }
 
 
